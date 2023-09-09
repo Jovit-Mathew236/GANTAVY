adeb74e410a653ff20599d4cb1630742f158fb75
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-09-09 23:18:17 +0530

    [FEAT] created Home Page section

---
+++ src/components/pages/HomePage/Home.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import SearchBar from '../../molecules/SearchBar'
+import styles from './Home.module.css'
+import RightArrow from '../../atom/svgs/RightArrow'
+
+function HomePage() {
+  return (
+    <div className={styles.homePage}>
+      <SearchBar />
+
+      <section>
+        <p>March 2023</p>
+        <div className={styles.cardsContainer}>
+
+          <div className={styles.card}>
+            <div className={styles.cardTopSection}>
+              <p className={styles.id}>#1203</p>
+              <p className={styles.date}>21 March 2023</p>
+            </div>
+            <div className={styles.cardMainSection}>
+              <p>Martin T Varghese</p>
+              <p>johnsnow@gmail.com</p>
+            </div>
+            <div className={styles.cardBottomSection}>
+              <div className={styles.countries}>
+                <p className={styles.country}>in</p>
+                <p className={styles.country}>au</p>
+                <p className={styles.country}>in</p>
+              </div>
+              <p>
+                <RightArrow />
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.cardTopSection}>
+              <p className={styles.id}>#1203</p>
+              <p className={styles.date}>21 March 2023</p>
+            </div>
+            <div className={styles.cardMainSection}>
+              <p>Martin T Varghese</p>
+              <p>johnsnow@gmail.com</p>
+            </div>
+            <div className={styles.cardBottomSection}>
+              <div className={styles.countries}>
+                <p className={styles.country}>in</p>
+                <p className={styles.country}>au</p>
+                <p className={styles.country}>in</p>
+              </div>
+              <p>
+                <RightArrow />
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section>
+        <p>March 2023</p>
+        <div className={styles.cardsContainer}>
+
+          <div className={styles.card}>
+            <div className={styles.cardTopSection}>
+              <p className={styles.id}>#1203</p>
+              <p className={styles.date}>21 March 2023</p>
+            </div>
+            <div className={styles.cardMainSection}>
+              <p>Martin T Varghese</p>
+              <p>johnsnow@gmail.com</p>
+            </div>
+            <div className={styles.cardBottomSection}>
+              <div className={styles.countries}>
+                <p className={styles.country}>in</p>
+                <p className={styles.country}>au</p>
+                <p className={styles.country}>in</p>
+              </div>
+              <p>
+                <RightArrow />
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default HomePage
