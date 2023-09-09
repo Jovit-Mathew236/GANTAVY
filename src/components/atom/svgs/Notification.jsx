b9d1ed009cfeabfe94422fa2fa54f43cda9bc6d1
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-09-09 23:19:07 +0530

    minor fix

---
+++ src/components/atom/svgs/Notification.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notification = () => {
+    return (
+        <svg width="15" height="15" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect y="-8.00006" width="42" height="48" fill="white" />
+        </svg>
+    )
+}
+
+export default Notification
