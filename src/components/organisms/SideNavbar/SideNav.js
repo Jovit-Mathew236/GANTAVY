f950be994592b255224a61560c8ade788fe115bd
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-09-12 11:12:51 +0530

    make menu items highlighted if it select

--- src/components/organisms/SideNavbar/SideNav.js
+++ src/components/organisms/SideNavbar/SideNav.js
@@ -1,11 +1,9 @@
-'use client'
+"use client";
 import React from "react";
 import styles from "./SideNavBar.module.css";
-// import { useRouter } from 'next/router'
 
 const SideNav = () => {
-  // const router = useRouter()
-  const url = "/"
+  const url = window.location.pathname;
   return (
     <nav className={styles.navBar}>
       <div className={styles.logo}></div>
@@ -13,28 +11,34 @@ const SideNav = () => {
       <div className={styles.menu}>
         <a
           href="/"
-          style={url === "/" && { color: "#141220", fontWeight: "bold" }}
+          style={url === "/" ? { color: "#141220", fontWeight: "bold" } : {}}
           className={styles.menuItem}
         >
           Home
         </a>
         <a
           href="/message"
-          // style={url === "/message" && { color: "#141220" }}
+          style={
+            url === "/message" ? { color: "#141220", fontWeight: "bold" } : {}
+          }
           className={styles.menuItem}
         >
           Message
         </a>
         <a
           href="/admin"
-          // style={url === "/admin" && { color: "#141220" }}
+          style={
+            url === "/admin" ? { color: "#141220", fontWeight: "bold" } : {}
+          }
           className={styles.menuItem}
         >
           Admin
         </a>
         <a
           href="/help"
-          // style={url === "/help" && { color: "#141220" }}
+          style={
+            url === "/help" ? { color: "#141220", fontWeight: "bold" } : {}
+          }
           className={styles.menuItem}
         >
           Help
