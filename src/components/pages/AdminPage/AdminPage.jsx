da7742ef8b008be1c4becc7727ffcf83a2100a05
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-09-12 11:13:04 +0530

    [FEAT] created admin page

--- src/components/pages/AdminPage/AdminPage.jsx
+++ src/components/pages/AdminPage/AdminPage.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import styles from './Admin.module.css'
+import Edit from '../../atom/svgs/Edit'
+import Delete from '../../atom/svgs/Delete'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className={styles.adminPage}>
+      <h1>Admin</h1>
+
+      <div className={styles.adminContainer}>
+        
+        <div className={styles.adminFiled}>
+          <input type="text" value={"jovitmathew236@gmail.com"} disabled />
+          <div className={styles.adminControl}>
+            <p><Edit/></p>
+            <p><Delete/></p>
+          </div>
+        </div>
+        
+        <div className={styles.adminFiled}>
+          <input type="text" value={"jovitmathew236@gmail.com"} disabled />
+          <div className={styles.adminControl}>
+            <p><Edit/></p>
+            <p><Delete/></p>
+          </div>
+        </div>
+        
+        <div className={styles.adminFiled}>
+          <input type="text" value={"jovitmathew236@gmail.com"} disabled />
+          <div className={styles.adminControl}>
+            <p><Edit/></p>
+            <p><Delete/></p>
+          </div>
+        </div>
+        
+      </div>
+
+      <button>Add new</button>
+    </div>
   )
 }
 
