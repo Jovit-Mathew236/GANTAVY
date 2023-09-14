e75356c5fe192c6df5dfac3a9923b18a19ade83e
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-09-14 12:44:02 +0530

    added add enw pop up

--- src/components/pages/AdminPage/AdminPage.jsx
+++ src/components/pages/AdminPage/AdminPage.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from './Admin.module.css'
 import Edit from '../../atom/svgs/Edit'
 import Delete from '../../atom/svgs/Delete'
 
 const AdminPage = () => {
+  const [popUp, setPopUp] = useState(false)
+  const handleCancelClick = () => {
+    setPopUp(false);
+  };
   return (
     <div className={styles.adminPage}>
+
+      {popUp && <div className={styles.addAdminPopUp}>
+        <div className={styles.popUpContainer}>
+          <h1>&nbsp;&nbsp;&nbsp; Add email</h1>
+          <input type="text" id="" name='email' placeholder="example@gmail.com" autoComplete="email" />
+          <div className={styles.btnS}>
+            <button onClick={handleCancelClick}>Cancel</button>
+            <button>Add</button>
+          </div>
+        </div>
+      </div>}
+
       <h1>Admin</h1>
 
       <div className={styles.adminContainer}>
 
         <div className={styles.adminFiled}>
-          <input type="text" id="" name='email' value={"jovitmathew236@gmail.com"} disabled autoComplete="email"/>
+          <input type="text" id="" name='email' value={"jovitmathew236@gmail.com"} disabled autoComplete="email" />
           <div className={styles.adminControl}>
             <p><Edit /></p>
             <p><Delete /></p>
@@ -19,7 +36,7 @@ const AdminPage = () => {
         </div>
 
         <div className={styles.adminFiled}>
-          <input type="text" id="" name='email' value={"jovitmathew236@gmail.com"} disabled autoComplete="email"/>
+          <input type="text" id="" name='email' value={"jovitmathew236@gmail.com"} disabled autoComplete="email" />
           <div className={styles.adminControl}>
             <p><Edit /></p>
             <p><Delete /></p>
@@ -27,7 +44,7 @@ const AdminPage = () => {
         </div>
 
         <div className={styles.adminFiled}>
-          <input type="text" id="" name='email' value={"jovitmathew236@gmail.com"} disabled autoComplete="email"/>
+          <input type="text" id="" name='email' value={"jovitmathew236@gmail.com"} disabled autoComplete="email" />
           <div className={styles.adminControl}>
             <p><Edit /></p>
             <p><Delete /></p>
@@ -37,9 +54,74 @@ const AdminPage = () => {
 
       </div>
 
-      <button>Add new</button>
+      <ClientButton setPopUP={setPopUp} />
     </div>
   )
 }
 
-export default AdminPage
+const ClientButton = ({ setPopUP }) => {
+  const handleAddNewClick = () => {
+    setPopUP(true);
+  };
+
+  return (
+    <button onClick={handleAddNewClick}>
+      <p>Add new</p>
+    </button>
+  );
+};
+// const CancelButton = ({ setPopUP }) => {
+//   const handleCancelClick = () => {
+//     setPopUP(false);
+//   };
+
+//   return (
+//     <button onClick={handleCancelClick}>Cancel</button>
+//   );
+// };
+
+export default AdminPage;
+
+
+
+
+// return (
+//   <div className={styles.adminPage}>
+//     <h1>Admin</h1>
+
+//     <div className={styles.adminContainer}>
+//       <AdminField email="jovitmathew236@gmail.com" />
+//       <AdminField email="another@example.com" />
+//       {/* Add more AdminField components as needed */}
+//     </div>
+
+//     <ClientButton />
+//   </div>
+// );
+
+// const AdminField = ({ email }) => {
+//   const handleEditClick = () => {
+//     console.log(`Editing ${email}`);
+//   };
+
+//   const handleDeleteClick = () => {
+//     console.log(`Deleting ${email}`);
+//   };
+
+//   return (
+//     <div className={styles.adminField}>
+//       <input
+//         type="text"
+//         value={email}
+//         disabled
+//         autoComplete="email"
+//       />
+//       <div className={styles.adminControl}>
+//         <p onClick={handleEditClick}><Edit /></p>
+//         <p onClick={handleDeleteClick}><Delete /></p>
+//       </div>
+//     </div>
+//   );
+// };
+
+
