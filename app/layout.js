8831f471588e9e1ffa881c363a5151e097613d99
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-10-08 13:09:00 +0530

    minor changes

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {
-        <body className={inter.className}>
+        <body >
           <SideNav />
           <main>
             <TopNav />
