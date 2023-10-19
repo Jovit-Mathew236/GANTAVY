cd5369e25c0a651bd95ac8f56c3f29d7611e5c01
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-10-19 20:27:20 +0530

    minor bug fix

--- src/components/molecules/Loading.jsx
+++ src/components/molecules/Loading.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './molecules.module.css'
+import React from 'react';
+import styles from './molecules.module.css';
 
 const Loading = () => {
   return (
@@ -7,28 +7,17 @@ const Loading = () => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        style={{
-          margin: 'auto',
-          background: 'rgb(255, 255, 255)',
-          display: 'block',
-          shapeRendering: 'auto',
-        }}
-        width="200px"
-        height="200px"
+        style={{ margin: 'auto', background: 'rgb(255, 255, 255)', display: 'block', shapeRendering: 'auto' }}
+        width="150px" height="150px"
         viewBox="0 0 100 100"
         preserveAspectRatio="xMidYMid"
       >
-        <circle cx="50" cy="50" r="0" fill="none" stroke="#e90c59" strokeWidth="2">
-          <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="0s"></animate>
-          <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="0s"></animate>
-        </circle><circle cx="50" cy="50" r="0" fill="none" stroke="#46dff0" strokeWidth="2">
-          <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.5s"></animate>
-          <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.5s"></animate>
-        </circle>
-
+        <path d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#f87920" stroke="none">
+          <animateTransform attributeName="transform" type="rotate" dur="1s" repeatCount="indefinite" keyTimes="0;1" values="0 50 51;360 50 51"></animateTransform>
+        </path>
       </svg>
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
